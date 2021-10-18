3e59208e6f44b8921ff423e63fbabd001712543e
Aaron Lu <aaronnoh0409@gmail.com>
2021-10-18 16:56:53 -0400

    footer is here!

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/nav";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from "./page/main";
+import Footer from './components/footer';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Route exact path="/" component={Main} />
+      <Footer />
   </Router>
   );
 }
---
+++ practice-app/src/components/footer/index.js
@@ -0,0 +1,94 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import "./style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Footer() {
+    return (
+        <div className="container">
+            <div className="stuff">
+                <ul>
+                    <li><strong>Stuff</strong></li>
+                    <li>Mac</li>
+                    <li>iPad</li>
+                    <li>iPhone</li>
+                    <li>Watch</li>
+                    <li>TV</li>
+                    <li>AirPods</li>
+                    <li>HomePod</li>
+                    <li>AirTag</li>
+                    <li>Accessories</li>
+                </ul>
+            </div>
+            <div className="stuff">
+                <ul>
+                    <li><strong>Stuff</strong></li>
+                    <li>Mac</li>
+                    <li>iPad</li>
+                    <li>iPhone</li>
+                    <li>Watch</li>
+                    <li>TV</li>
+                    <li>AirPods</li>
+                    <li>HomePod</li>
+                    <li>AirTag</li>
+                    <li>Accessories</li>
+                </ul>
+            </div>
+            <div className="stuff">
+                <ul>
+                    <li><strong>Stuff</strong></li>
+                    <li>Mac</li>
+                    <li>iPad</li>
+                    <li>iPhone</li>
+                    <br></br>
+                    <li><strong>Stuff</strong></li>
+                    <li>Watch</li>
+                    <li>TV</li>
+                    <li>AirPods</li>
+                    <br></br>
+                    <li><strong>Stuff</strong></li>
+                    <li>HomePod</li>
+                    <li>AirTag</li>
+                    <li>Accessories</li>
+                </ul>
+            </div>
+            <div className="stuff">
+                <ul>
+                    <li><strong>Stuff</strong></li>
+                    <li>Mac</li>
+                    <li>iPad</li>
+                    <li>iPhone</li>
+                    <br></br>
+                    <li><strong>Stuff</strong></li>
+                    <li>Watch</li>
+                    <li>TV</li>
+                    <br></br>
+                    <li><strong>Stuff</strong></li>
+                    <li>AirPods</li>
+                    <li>HomePod</li>
+                    <li>AirTag</li>
+                    <li>Accessories</li>
+                </ul>
+            </div>
+            <div className="stuff">
+                <ul>
+                    <li><strong>Stuff</strong></li>
+                    <li>Mac</li>
+                    <li>iPad</li>
+                    <li>iPhone</li>
+                    <li>Watch</li>
+                    <br></br>
+                    <li><strong>Stuff</strong></li>
+                    <li>TV</li>
+                    <li>AirPods</li>
+                    <li>HomePod</li>
+                    <li>AirTag</li>
+                    <li>Accessories</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
